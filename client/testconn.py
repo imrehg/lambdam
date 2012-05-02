5f51d68ee16bdc67031e274a496ce83fb44c89ad
Gergely Imreh <imrehg@gmail.com>
2012-05-02 16:35:55 +0800

    update python sender client for new data structure

--- client/testconn.py
+++ client/testconn.py
@@ -1,9 +1,21 @@
+from time import sleep
+from random import gauss, shuffle
 from socketIO import SocketIO
 s = SocketIO('localhost', 5000)
 
+numchn = 6
+vals = [350.0 for i in range(numchn)]
+
 while True:
-    msg = raw_input("What message to send? ")
-    if len(msg) == 0:
+    try:
+        order = range(numchn)
+        shuffle(order)
+        for i in order:
+            vals[i] += gauss(0, 0.1)
+            out = {"wavelength" : { "channel": i+1, "value": vals[i]}}
+            print out
+            s.emit('message', out)
+            sleep(0.05)
+    except (KeyboardInterrupt):
+        print("Stopping")
         break
-    else:
-        s.emit('message', {'text': msg})
