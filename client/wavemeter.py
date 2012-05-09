5f1a97140515bdd220930b49a1278b7d4768d536
Gergely Imreh <imrehg@gmail.com>
2012-05-09 11:38:32 +0800

    working communication with Windows workarounds

--- client/wavemeter.py
+++ client/wavemeter.py
@@ -8,11 +8,18 @@ import asyncore, socket
 from time import sleep
 from random import gauss, shuffle
 from socketIO import SocketIO
+import logging
+import os
 
 try:
     import simplejson as json
 except(ImportError):
     import json
+
+FORMAT = '%(asctime)-15s | %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('wavemeter')
+logger.setLevel(logging.DEBUG)
 
 settingsQ = mp.Queue()
 readingsQ = mp.Queue()
@@ -22,25 +29,32 @@ class RemoteClient(asyncore.dispatcher):
     def __init__(self, host, path, rQ, sQ):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.mysocket = SocketIO('localhost', 5000)
+        self.connect(('localhost', 3000))  # needs it on windows otherwise doesn't write
+        self.mysocket = SocketIO('localhost', 3000)
         self.rQ = rQ
         self.sQ = sQ
 
     def handle_connect(self):
+        logger.debug("RC connected")
         pass
 
     def handle_close(self):
-        print "Close?"
-        pass
+        logger.debug("RC Closing")
 
     def readable(self):
-        return True
+        """ Check if data is readable """
+        workaround = os.name in ['nt']
+        reading = not workaround
+        logger.debug("RC checking readable, %s", reading)
+        if workaround:
+            # call manually the read function, because somehow
+            # on Windows this doesn't get called even if the
+            # function returns True
+            self.handle_read()
+        return reading
 
     def handle_read(self):
-        # print "Reading"
-        # print "Reading", 
-        # self.mysocket.emit('message', self.rQ.get())
-        # self.serial = 0
+        logger.debug("RC reading data")
         try:
             data = self.mysocket.recv()
             try:
@@ -56,26 +70,17 @@ class RemoteClient(asyncore.dispatcher):
                         raise
             except:
                 raise
-            # if data['settings']:
-            #     print data['settings']
         except(socket.timeout):
             pass
 
     def writable(self):
-        # return True
+        logger.debug("RC checking writable, %s", (not self.rQ.empty()))
         return not self.rQ.empty()
 
     def handle_write(self):
+        logger.debug("RC writing data")
         while not self.rQ.empty():
             self.mysocket.emit('message', self.rQ.get())
-        # writ = random.random() < 0.01
-        # if writ:
-        # print "New setting"
-        # self.sQ.put({'num': random.randint(1, 11)})
-        # # self.rQ.put({'set': self.serial})
-        # # self.serial += 1
-        # print "Written"
-        pass
 
 class Wavemeter(threading.Thread):
 
@@ -86,8 +91,8 @@ class Wavemeter(threading.Thread):
         self.interval = interval
         self.sQ = sQ
         self.rQ = rQ
-        # self.settings = {'channels' : [{'num': 1, 't': 0.01}]}
-        self.settings = {'channels' : []}
+        self.settings = {'channels' : [{'num': 3, 't': 0.01}]}
+        # self.settings = {'channels' : []}
         self.done = threading.Event()
         self.numchn = 16+1
         self.vals = [350.0 for i in range(self.numchn)]
@@ -100,7 +105,7 @@ class Wavemeter(threading.Thread):
             now = time()
             if not self.sQ.empty():
                 self.settings = self.sQ.get()
-            print self.settings
+            # print self.settings
             for ch in self.settings['channels']:
                 i = int(ch['num'])
                 t = float(ch['t'])
@@ -111,19 +116,15 @@ class Wavemeter(threading.Thread):
                 timestamp = time()
                 self.rQ.put({"wavelength" : { "channel": i, "value": self.vals[i], "timestamp": timestamp}})
             self.done.wait(self.interval)
-            print time()-now
+            # print time()-now
         
 client = RemoteClient('www.python.org', '/', readingsQ, settingsQ)
-# daemon = True
-# thr = threading.Thread(target=asyncore.loop, kwargs={"timeout": 0.01})
-# thr.start() # will run "foo"
-# asyncore.loop(timeout=0.01)
-wavemeterThread = Wavemeter(0.001, settingsQ, readingsQ)
+wavemeterThread = Wavemeter(0.5, settingsQ, readingsQ)
 wavemeterThread.start()
 
 while True:
     try:
-        asyncore.poll(timeout=0.001)
+        asyncore.poll(timeout=0.5)
     except (KeyboardInterrupt):
         client.close()
         wavemeterThread.close()
